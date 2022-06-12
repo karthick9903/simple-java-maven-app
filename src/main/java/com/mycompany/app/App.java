6fa0905ef8039eaec99c69308b15e5466445fb63
karthick9903 <105573155+karthick9903@users.noreply.github.com>
2022-06-12 17:30:53 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
 
     public App() {}
 
-    public static void main(String[] args) {
+   /** public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
 
@@ -19,3 +19,4 @@ public class App
     }
 
 }
+*/
